9efc923cb4bdaba83c4048fd0fe4903752738363
Jeremy Wood <js.wood206@gmail.com>
2021-02-17 21:33:11 -0800

    commit tree 292ed26b1887edba9940f3e658965b3129fa08ce

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(stringNames, eventName) {
+    let messages = [];
+    for (let i = 0; i < stringNames.length; i++) {
+        messages.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number)
+        number --
+    }
+}
